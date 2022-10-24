9236f651ed4f91d91df841930865dfb447540399
charlestian23 <charles.tian23@gmail.com>
2022-10-24 19:39:53 -0400

    Implemented toggle grid lines

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const DEFAULT_PEN_COLOR = '#333333'
 const DEFAULT_BACKGROUND_COLOR = '#000000'
 const DEFAULT_MODE = 'color'
-const DEFAULT_SHOW_GRID_LINES = false
-const DEFAULT_SIZE = 15
+const DEFAULT_SHOW_GRID_LINES = true
+const DEFAULT_ROWS = 15
+const DEFAULT_COLUMNS = 15
 
 let currentPenColor = DEFAULT_PEN_COLOR
 let currentBackgroundColor = DEFAULT_BACKGROUND_COLOR
 let currentMode = DEFAULT_MODE
 let currentShowGridLines = DEFAULT_SHOW_GRID_LINES
-let currentSize = DEFAULT_SIZE
+let currentRows = DEFAULT_ROWS
+let currentColumns = DEFAULT_COLUMNS
 
 function setPenColor(newPenColor) {
     currentPenColor = newPenColor
@@ -23,8 +25,51 @@ function setMode(newMode) {
     currentMode = newMode
 }
 
-function setSize(newSize) {
-    currentSize = newSize
+function setShowGridLines(newShowGridLines) {
+    currentShowGridLines = newShowGridLines
+}
+
+function setRows(newRows) {
+    currentRows = newRows
+}
+
+function setColumns(newColumns) {
+    currentColumns = newColumns
+}
+
+const brushColorPicker = document.getElementById("brushColorPicker")
+brushColorPicker.onInput = (e) => setPenColor(e.target.value)
+
+const backgroundColorPicker = document.getElementById("backgroundColorPicker")
+backgroundColorPicker.onInput = (e) => setBackgroundColor(e.target.value)
+
+const gridLinesButton = document.getElementById("gridLinesButton")
+gridLinesButton.onclick = () => toggleGridLines()
+function toggleGridLines() {
+    currentShowGridLines = !currentShowGridLines
+    setGridLines()
+}
+
+function setGridLines() {
+    let squares = document.querySelectorAll(".square")
+    for (let i = 0; i < squares.length; i++) {
+        console.log(squares)
+        if (currentShowGridLines)
+            squares[i].style.border = "0.1rem solid black"
+        else
+            squares[i].style.border = "none"
+    }
+    if (!currentShowGridLines)
+    {
+        for (let i = 0; i < currentColumns; i++) {
+            squares[i].style.borderTop = "0.1rem solid black";
+            squares[squares.length - 1 - i].style.borderBottom = "0.1rem solid black";
+        }
+        for (let i = 0; i < currentRows; i++) {
+            squares[i * currentColumns].style.borderLeft = "0.1rem solid black";
+            squares[(i + 1) * currentColumns - 1].style.borderRight = "0.1rem solid black";
+        }
+    }
 }
 
 
@@ -42,6 +87,8 @@ function setupGrid(rows, columns) {
             grid.appendChild(square)
         }
     }
+
+    setGridLines()
 }
 
-setupGrid(DEFAULT_SIZE, DEFAULT_SIZE);
+setupGrid(DEFAULT_ROWS, DEFAULT_COLUMNS);
